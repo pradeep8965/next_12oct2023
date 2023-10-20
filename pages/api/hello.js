45dbcc063a3cb776f66cdb9381d278f938f33784
“misterpp143@gmail.comgit config --global user.email misterpp143@gmail.comgit config  credential.helper ‘store’git config --global user.name “misterpp143@gmail.com <misterpp143@gmail.com>
2023-10-20 15:16:24 +0530

    c1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
+//1. import Area
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+//2. Defination Area
+ function handler(req, res) {
+          //function serialization 
+  //object.method().method(actualargument);
+                      //{property:value}
+  res.status(200).json({
+                           msg1:`Hello1 ${req.query.name}`,
+                           msg2:'Hello2',
+                           msg3:"Hello3" ,
+                           msg4:`Hello4 ${req.query.surname}`,
+                           //obj.property.property
+                           testkey:req.query.name
+                        })
 }
+ 
+//3. Export Area
+export default handler;
